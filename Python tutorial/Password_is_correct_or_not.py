006f5ce16b511a2078e91eb2f91233785f36edb7
Vaishnavi-Pandit <vaishnavipandit242@gmail.com>
2024-01-19 08:59:26 +0530

    if demo

---
+++ Python tutorial/Password_is_correct_or_not.py
@@ -0,0 +1,8 @@
+# To check password is correct or not.
+Passorig="Vaish_123"
+passw=input('Enter the password')
+if passw==Passorig:
+    print('Correct password')
+    print ('Welcome')
+else:
+    print('incorrect password')
