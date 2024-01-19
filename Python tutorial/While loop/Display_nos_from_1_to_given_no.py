c5f6fa8b3fd6ce6f80a16469afe7f1c6e62c9af8
Vaishnavi-Pandit <vaishnavipandit242@gmail.com>
2024-01-19 13:58:05 +0530

    while programs

---
+++ Python tutorial/While loop/Display_nos_from_1_to_given_no.py
@@ -0,0 +1,6 @@
+#display no from 1 to 10
+n=int(input('enter a no'))
+p=1
+while p<=n:
+    print(p)
+    p=p+1
---
+++ Python tutorial/While loop/print_nos_in_decending_order.py
@@ -0,0 +1,5 @@
+#display nos from 10 to 1 in decending order
+n=10
+while n>=1:
+    print(n)
+    n=n-1
