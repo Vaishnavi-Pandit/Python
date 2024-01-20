01b0b760c1a2dd533aab90a7d31f225c263b86ac
Vaishnavi-Pandit <vaishnavipandit242@gmail.com>
2024-01-20 08:30:03 +0530

    for loop

---
+++ Python tutorial/for loop/to-display-nos-from-1-to-10.py
@@ -0,0 +1,5 @@
+#to display nos from 1 to 10
+r=range(1,11)
+for i in r:
+    print(i)
+    print('yes')
--- Python tutorial/input in python/input.py
+++ Python tutorial/input in python/input.py
@@ -1,3 +1,3 @@
 n=float(input('Enter a number: '))
 print("u entered=%f" %n)    #this will write only 6 digits after point
-print("u entered={}".format(n))  t#this will write all digits after point
+print("u entered={}".format(n))  #this will write all digits after point
