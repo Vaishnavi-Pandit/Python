8d5e30dccf0b21508145bbb5963cea775cd18a11
Vaishnavi-Pandit <vaishnavipandit242@gmail.com>
2024-01-29 10:10:39 +0530

    oops

---
+++ Python tutorial/Object Oriented Programming language/Duck-Typing.py
@@ -0,0 +1,14 @@
+#Duck Typing: Executing method without knowing its type.
+
+class Duck:
+    def talk(self):
+        print('Quack quack!')
+class Dog:
+    def talk(self):
+        print('Bow bow!')
+def call_talk(obj):
+    obj.talk() #without knowing type calling method.(duck typing)
+d=Duck()
+call_talk(d)
+t=Dog()
+call_talk(t)
